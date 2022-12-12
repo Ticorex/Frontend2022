997e76731e7cf6e9c749e5a0d2f2b0f00db7547a
Ticorex <70647911+Ticorex@users.noreply.github.com>
2022-12-12 15:12:56 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
 // JavaScript Document
 console.log("hi");
+
+//dit maakt de text " Night_mode_knop" een variable
+var Night_mode_knop;
+
+//dit zegt welk stukje html de variable representeert
+Night_mode_knop = document.querySelector("button:last-of-type");
+
+// als je night_mode_knop, in dit geval "button:last-of-type" aan klikt, runt de functie "Nightmode"
+Night_mode_knop.addEventListener("click", Nightmode)
+
+function Nightmode(){
+    Night_mode_knop.classlist.toggle("white");
+}
